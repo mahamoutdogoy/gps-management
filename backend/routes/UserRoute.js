1244564454ed4922a5a4e503c0e2a8b6968467d2
mahamoutdogoy <32535223+mahamoutdogoy@users.noreply.github.com>
2023-08-07 14:30:09 +0100

    add user routes

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    getUserCount,
+    upload
+} from "../controllers/Users.js";
+import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
+
+const router = express.Router();
+  
+ 
+router.get('/users', verifyUser, adminOnly, getUsers);
+router.get('/users/count', verifyUser, adminOnly, getUserCount);
+router.get('/users/:id', verifyUser, adminOnly, getUserById);
+router.post('/users', verifyUser, adminOnly, upload, createUser);
+router.patch('/users/:id', verifyUser, adminOnly, updateUser);
+router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
+
+export default router;
